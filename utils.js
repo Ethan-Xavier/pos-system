dad3894a9ad335d226cf42113edc31a7cf646fc2
Ethan-Xavier <ethanxavier777@gmail.com>
2025-11-26 13:08:00 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,9 @@
+// Shortcut to select element by id
+const $ = id => document.getElementById(id);
+
+// Generate unique ID
+function uid(){ return "ID-" + Math.random().toString(36).slice(2,9); }
+
+// Format price
+function formatPrice(p){ return typeof p==='string'?p:`$${p}`; }
 
